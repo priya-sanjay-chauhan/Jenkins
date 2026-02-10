afbec067f9272553660d81cf089c984656b153a4
priya-sanjay-chauhan <priyachauhan0145@gmail.com>
2026-02-10 21:33:37 +0530

    update commit

--- Demo.java
+++ Demo.java
@@ -1,5 +1,32 @@
-public class Demo{
-   public static void main(String[] args) {
-      System.out.println("Hello Jenkins");
-   }
+// public class Demo{
+//    public static void main(String[] args) {
+//       System.out.println("Hello Jenkins");
+//    }
+// }
+
+
+
+class Demo {
+
+    // Method to check whether a number is even or odd
+    static void checkEvenOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Program started");
+
+        int a = 10;
+        int b = 7;
+
+        checkEvenOdd(a);
+        checkEvenOdd(b);
+
+        System.out.println("Program ended");
+    }
 }
